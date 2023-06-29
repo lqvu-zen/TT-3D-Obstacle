57b7f172b1f341d6ed857a8dfc32b9ab23f0695d
Quang-Vu Le <ktvn100@gmail.com>
2023-06-29 21:37:34 +0700

    add player and ground materials

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private float xValue = 0f;
+    [SerializeField] private float yValue = 0f;
+    [SerializeField] private float zValue = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
